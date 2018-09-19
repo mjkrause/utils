227d55112c5c76eec0846d25f282515daacb0c0f
Michael Krause <mkrause1@ucsc.edu>
2018-09-19 11:14:25 -0700

    Cleaned up code to adhere to the 80 char rule

--- upload_multi_files.sh
+++ upload_multi_files.sh
@@ -2,47 +2,53 @@
 
 # DCCPC End-of-Pilot phase demo in Oct 2018.
 #
-# This script copies files from Seven Bridges located in an AWS S3 bucket to a Google bucket.
-# I had been given a list of presigned URLs that point and give access to these files.
+# This script copies files from Seven Bridges located in an AWS S3 bucket to
+# a Google bucket. I had been given a list of presigned URLs that point and
+# give access to these files.
 
 # This is an attempt to copy CRAM and CRAI files from a presigned URL to
 # Google bucket named commons-demo.
 
 # INPUTS:
 #   $1: a text file with presigned URLs, one URL per line
-#   $2: a master file that contains information to filter whether an object should be copied or not
+#   $2: a master file that contains information to filter whether an object
+#       should be copied or not
+#   $3: a string containing the full stack name, but it actually denotes the
+#       name of the directory in bucket "commons-dev", so make sure a directory
+#       with whatever name you use exists (this script isn't checking for it)
 
 # Invoke like so:
-#   ./upload_files.sh file_with_urls.txt
+#   ./upload_files.sh file_with_urls.txt gtex-wgs.txt $fullstack_name
 
-# Create list the only contains the relevant lines for the specific stack.
-#list=$(awk '$8 == "Xenon" || $8 == "All"' $2)
+
+num_cores=grep -c ^processor /proc/cpuinfo
 counter=0
-fullstack_name="Xenon"
 
 while read url; do
-    # We want the same base name for cram and crai files, they should only differ in their
-    # file extension. This is a crude way of achieving this.
+    # We want the same base name for cram and crai files, they should only
+    # differ in their file extension. This is a crude way of achieving this.
     if [[ $url = *"crai"* ]]; then
 	objectname=$( echo "$url" | cut -b84-123 )
     else
 	objectname=$( echo "$url" | cut -b84-118 )
     fi
     #echo $objectname
-    # "objectname" has a substring ".recab". In order to string-match objectname to strings
-    # in file "gtex-wgs.tsv" we need to remove that substring by slicing.
+    
+    # "objectname" has a substring ".recab". In order to string-match objectname
+    # to strings in file "gtex-wgs.tsv" we need to remove that substring by
+    # slicing.
     sub1=${objectname:0:24}
     sub2=${objectname:30}
     sub="$sub1$sub2"
     #echo "$sub"
 
-    # Return (if any) the line number of objectname in the master file "gtex-wgs.tsv", which is
-    # the second input argument to this script.
+    # Return (if any) the line number of objectname in the master file
+    # "gtex-wgs.tsv", which is the second input argument to this script.
     line_num=$(sed -n "/.*$sub.*/=" $2)
     #echo $line_num
 
-    # Use line number to retrieve the line in the master file (second input argument), which
-    # corresponds to the the file in objectname.
+    # Use line number to retrieve the line in the master file (second input
+    # argument), which corresponds to the the file in objectname.
     matched_line=$(sed -n -e "$line_num"p $2)
     #echo $matched_line"\n"
 
@@ -57,8 +63,7 @@ while read url; do
 	echo "Copying $objectname to Google bucket /commons-demo"
 	#echo $url
 	# Stream the output of curl to gsutil.
-	#curl "${url}" | gsutil -m cp -I gs://commons-demo/xenon2/$objectname
-	curl "${url}" | gsutil cp - gs://commons-demo/xenon2/$objectname &
+	curl "${url}" | gsutil cp - gs://commons-demo/$fullstack_name/$objectname &
     fi
     #sleep 1
     
