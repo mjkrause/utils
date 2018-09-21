de65174c41e7317f99fed03238d9d0d19ac8967d
Michael Krause <mkrause1@ucsc.edu>
2018-09-20 17:03:58 -0700

    Computes variables of string indices to cut new name from old name

--- xenon_rename_files.sh
+++ xenon_rename_files.sh
@@ -14,18 +14,29 @@ echo "Renaming $num_files files..."
 
 counter=0  # counts all processed files
 
-for filepath in $1/*.*; do
+# From https://stackoverflow.com/questions/5031764/position-of-a-string-within-a-string-using-linux-shell-script.
+strindex() {
+  x="${1%%$2*}"
+  [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
 
+for filepath in $1/*.*; do
     echo "File path: $filepath"
 
     filename=$(echo "$filepath" | sed "s/.*\///")
+    start=$(strindex "$filename" "GTEX")
+    start=$((start+1))
+    echo "Start index of new filename: $start"
+    # Use hard-coded length of two strings to get stop indices.
+    stop_crai=$((start + 39))
+    stop_cram=$((start + 34))
 
     # We want the same base name for cram and crai files, they should only
     # differ in their file extension. This is a crude way of achieving this.
     if [[ $filename = *"crai"* ]]; then
-	objectname=$( echo "$filename" | cut -b38-77 )
+	objectname=$( echo "$filename" | cut -b$start-$stop_crai )
     else
-	objectname=$( echo "$filename" | cut -b38-72 )
+	objectname=$( echo "$filename" | cut -b$start-$stop_cram )
     fi
 
     counter=$((counter+1))
