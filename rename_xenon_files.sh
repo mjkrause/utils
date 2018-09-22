29f49126416cfa50e2141950d9c9975a64d94369
Michael Krause <mkrause1@ucsc.edu>
2018-09-21 21:19:44 -0700

    Added progress bar

--- rename_xenon_files.sh
+++ rename_xenon_files.sh
@@ -20,7 +20,28 @@ strindex() {
   [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
 }
 
+# INPUT
+# $1:  current state
+# $2:  total state
+function ProgressBar {
+    # Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    # Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:                           
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+
+}
+
 for filepath in $1/*.*; do
+
+    ProgressBar ${counter} ${num_files}
     #echo "File path: $filepath"
 
     oldname=$(echo "$filepath" | sed "s/.*\///")
