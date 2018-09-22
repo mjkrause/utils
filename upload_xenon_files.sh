17d0ed64e9bcb1137c0d6788b1605cb333956277
Michael Krause <mkrause1@ucsc.edu>
2018-09-22 10:43:44 -0700

    Implemented gsutil cp command to upload all files in input folder

---
+++ upload_xenon_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# DCCPC End-of-Pilot phase demo in Oct 2018.
+#
+# The script uploads CRAI and CRAM from a folder to a Google bucket.
+
+# INPUTS:
+#   $1: path to the folder the files reside in (this will create a new
+#       subdirectory in the bucket with the same name)
+#       WHAT IF THAT SUBDIRIRECTORY ALREADY EXISTS - DOES IT APPEND TO IT?
+
+start_time=$SECONDS
+num_files=$(ls $1 | wc -l)  # total number of files to process
+echo "Uploading $num_files files..."
+dest_bucket=gs://commons-demo/xenon_final
+
+# Upload command.
+#   -m: upload multiple files (some for of parllel processing)
+#   -r: recurse into $1
+gsutil -m cp -r $1 $dest_bucket
+
+echo "...done uploading $num_files files to folder $1"
+
+end_time=$SECONDS
+execution_time=$(($((end_time-$start_time)) / 60))
+echo "That took $execution_time min"
