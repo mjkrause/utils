d10677b00b1d04ab99fbae0b4abba67f9a295b34
Michael Krause <mkrause1@ucsc.edu>
2018-09-22 21:27:41 -0700

    Changed name of helium download script

---
+++ download_helium_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Download files from Team Helium
+
+# 1. log at https://helium.commonsshare.org/
+# 2. in ~/dccpc/data-commons-workspace/bin create a Python 3.6 virtual environment
+# 3. pip-install packages requests and six
+# 4. run `python helium.py login
+# 5. this returns a URL, paste it in the browser
+# 6. bach in the terminal this should print "Detected login for user..."
+#    together with a prompt, answer "y"
+# 7. this should return the bearer token, which should be valid for > 12 h
+
+# INPUTS
+#  $1:  bearer token
+#  $2:  GUID
+
+
+#curl -v --header "Authorization: Bearer $token" "https://helium.commonsshare.org/dosapi/dataobjects/cce38e8723a24ce38ec8f8d0fb1beddb/"
+
+url=https://helium.commonsshare.org/dosapi/dataobjects
+
+resp=curl --header "Authorization: Bearer $1" "$url/$2/"
+
+echo $resp
