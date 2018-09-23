e89acf0b334209145d606ad15a7cf58cf35e4f7e
Michael Krause <mkrause1@ucsc.edu>
2018-09-22 21:29:06 -0700

    Deleted old helium download script

--- download_helium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# https://helium.commonsshare.org/dosapi/dataobjects/2a7f9eb4878f4f6ca4464c794f8eaf95/
-
-url=https://helium.commonsshare.org/dosapi/dataobjects
-
-curl -v --header "Authorization: Bearer $1" "$url/$2/"
