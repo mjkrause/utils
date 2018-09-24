147e8f36859b6b8c849aa6664f32ca769125b4b9
Michael Krause <mkrause1@ucsc.edu>
2018-09-24 14:08:33 -0700

    Added checksum integrity 3-times retry

--- download_helium_files.sh
+++ download_helium_files.sh
@@ -13,34 +13,65 @@
 # 7. this should return the bearer token, which should be valid for > 12 h
 
 # INPUTS
-#  $1:  bearer token
-#  $2:  GUID
+#  $1:  TSV file with list of GUIDs
+#  $2:  string of bearer token
 #  $3:  destination path where files will be downloaded to
 
 # EXAMPLE:
 # curl -v --header "Authorization: Bearer $token" "https://helium.commonsshare.org/dosapi/dataobjects/cce38e8723a24ce38ec8f8d0fb1beddb/"
 
 
-function download_guids() {
+function download_guid() {
+    # Download file with specific GUID to specific folder. If downloaded file
+    # is corrupted retry up to 3 times. All inputs are string variables.
     
+    # $1:  GTEX file name
+    # $2:  DOS GUID
+    # $3:  bearer token for access
+    # $4:  destination directory for download
+
+    #$token=$3
+    #$dos_guid=$2
+    #$download_dir=$4
+
+    retry_counter=0 
     url=https://helium.commonsshare.org/dosapi/dataobjects
-    resp=$(curl --header "Authorization: Bearer $1" "$url/$2/")
+    resp=$(curl --header "Authorization: Bearer $3" "$url/$2/")
     echo $resp | python -m json.tool  # pretty-prints JSON
 
     download_url=$(echo $resp | jq -r '.urls[].url')
-    cd $3
-    curl -L -O --header "Authorization: Bearer $1" "$download_url"
+    checksum_real=$(echo $resp | jq -r '.urls[].checksum')
+    cd $4
+    curl -L -O --header "Authorization: Bearer $3" "$download_url"
+    checksum_test=$($(echo sha256sum $1) | awk '{ print $1 }')
+    echo "Test checksum: $checksum_test"
     cd -;  # navigate back to original directory
+    # If checksum of downloaded file doesn't match the real checksum, try again.
+    if ! [ $checksum_real = $checksum_test ]; then
+	if [ $retry_counter < 4 ]; then
+	    download_guids $1 $2 $3 $4  # recurse to retry
+	    retry_counter=$((retry_counter+=1))
+	else
+	    echo "Retried downloading $retry_counter times - file with DOS GUID $2 is corrupted"
+	fi
+    fi
 }
+
+# Log output.
+#LOG_LOCATION=/home/user/scripts/logs
+#exec > >(tee -i $LOG_LOCATION/logfile.txt)
+exec > >(tee -i logfile.txt)
+exec 2>&1
 
 
 while read line; do
     echo $line
 
+    gtex_filename=$(echo $line | awk '{print $1}')
     dos_guid=$(echo $line | awk '{print $2}')
 
     echo $dos_guid
 
-    download_guids $1 $dos_guid $3
+    download_guid $gtex_filename $dos_guid $2 $3
 
 done < $1
